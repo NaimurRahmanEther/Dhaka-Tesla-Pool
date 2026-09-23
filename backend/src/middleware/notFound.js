b6111102490e8083cd200b52f80afc5b78ffafa0
NaimurRahmanEther <naimurrahmanether@gmail.com>
2026-09-23 14:19:48 +0600

    feat: add centralized error handling

---
+++ backend/src/middleware/notFound.js
@@ -0,0 +1,13 @@
+const AppError = require("../utils/AppError")
+
+
+const notFound=(req,res,next)=>{
+    next(
+        new AppError(`Request not found:${req.method} ${req.originalUrl}`,
+            404
+
+         )
+    )
+}
+
+module.exports=notFound
