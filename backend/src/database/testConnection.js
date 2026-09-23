d3aedb81f7fce05c84b6e26a84326e8f71980b26
NaimurRahmanEther <naimurrahmanether@gmail.com>
2026-09-23 11:13:42 +0600

    chore: setup backend server and database

---
+++ backend/src/database/testConnection.js
@@ -0,0 +1,20 @@
+
+const pool=require('./db')
+
+async function testConnection() {
+
+    try {
+        const result=await pool.query(
+            "SELECT NOW()"
+        )
+        console.log("Database connected",result.rows[0])
+        
+    } catch (error) {
+        console.error(
+            "Database connection failed", error.message
+        )
+    }
+    
+}
+
+module.exports=testConnection
