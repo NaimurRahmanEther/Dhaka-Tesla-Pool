4e5621849290393b4886e678eb209c6a8e87d380
NaimurRahmanEther <naimurrahmanether@gmail.com>
2026-09-24 23:02:04 +0600

    feat(location): implement location module and road edges

---
+++ backend/src/modules/location/location.routes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+const controller = require("./location.controller");
+
+router.get(
+  "/",
+
+  controller.getAllLocations,
+);
+
+router.get(
+  "/:id",
+
+  controller.getLocationById,
+);
+
+module.exports = router;
---
+++ backend/src/modules/location/location.service.js
@@ -0,0 +1,23 @@
+const locationRepository = require("./location.repository");
+
+const AppError = require("../../utils/AppError");
+
+const getAllLocations = async () => {
+  return locationRepository.findAllLocations();
+};
+
+const getLocationById = async (id) => {
+  const location = await locationRepository.findLocationById(id);
+
+  if (!location) {
+    throw new AppError("Location not found", 404);
+  }
+
+  return location;
+};
+
+module.exports = {
+  getAllLocations,
+
+  getLocationById,
+};
